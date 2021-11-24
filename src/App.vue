c1b395dcd1a4f4b1569783b9d70cf75910bbbb94
martin <zhengxi.li.19@neoma-bs.com>
2021-11-25 00:34:11 +0100

    Dev: - Include jquery, boostrap, adminLte3 and some static assets - Integrate with the origin projet page

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,67 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/DeploimentHistory">DeploimentHistory</router-link> |
-     <router-link to="/projet">Projet</router-link>
-  </div>
-  <router-view/>
+  <body class="hold-transition sidebar-mini layout-fixed">
+    <div class="wrapper">
+      <Preloader></Preloader>
+      <Navbar></Navbar>
+      <Sidebar></Sidebar>
+
+      <div class="content-wrapper">
+        <!-- <ContentHeader></ContentHeader>
+
+        <section class="content">
+        
+        </section> -->
+        <div class="container-fluid">
+          <router-view />
+        </div>
+      </div>
+
+      <Footer></Footer>
+      <ControlSidebar></ControlSidebar>
+    </div>
+  </body>
 </template>
 
+<script>
+import $ from "jquery";
+import "../node_modules/admin-lte/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css";
+import "../node_modules/admin-lte/plugins/select2/css/select2.min.css";
+
+import Preloader from "./dashboard/Preloader.vue";
+//import ContentHeader from "./dashboard/ContentHeader.vue"
+//import ExampleForm from "./components/ExampleForm.vue"
+import Navbar from "./dashboard/Navbar.vue";
+import Sidebar from "./dashboard/Sidebar.vue";
+import Footer from "./dashboard/Footer.vue";
+import ControlSidebar from "./dashboard/ControlSidebar.vue";
+
+export default {
+  name: "App",
+  components: {
+    Preloader,
+    // ContentHeader,
+    // ExampleForm,
+    Navbar,
+    Sidebar,
+    Footer,
+    ControlSidebar,
+  },
+  methods: {
+    initializeSelect2() {
+      //Initialize Select2 Elements
+      $(".select2").select2();
+
+      //Initialize Select2 Elements
+      $(".select2bs4").select2({
+        theme: "bootstrap4",
+      });
+    },
+  },
+  mounted() {
+    this.initializeSelect2();
+  },
+};
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+@import url("https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback");
 </style>
